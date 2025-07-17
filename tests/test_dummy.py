9e2906416ac4227c707ae9e99ccc6a6d8efd1c97
Emil Madsen <gitglobalemail.alright990@passinbox.com>
2025-07-17 17:35:58 +0200

    chore: [#64729] add integration-test

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+import pytest
 
 
 def test_dummy1() -> None:
@@ -7,4 +8,14 @@ def test_dummy1() -> None:
 
 
 async def test_dummy2() -> None:
+    pass
+
+
+@pytest.mark.integration_test
+def test_dummy3() -> None:
+    pass
+
+
+@pytest.mark.integration_test
+async def test_dummy4() -> None:
     pass
